9d4bec39ad66d98ee670eb5528fc3a90b2a440f0
Roxana Solano <roxansolan@gmail.com>
2025-05-28 21:32:46 +0200

    add indentation to the app file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,49 @@
-import '../styles/App.scss';
-
+import "../styles/App.scss";
 
 function App() {
   return (
-    <p>Escribe a <header>
-      <h1>¡Cuidado con Grogu!</h1>
-    </header>
-    <main class="page">
-      <section class="board">
-        <div class="cell"><div class="grogu">👣</div></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-      </section>
+    <>
+      <header>
+        <h1>¡Cuidado con Grogu!</h1>
+      </header>
+      <main class="page">
+        <section class="board">
+          <div class="cell">
+            <div class="grogu">👣</div>
+          </div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+        </section>
 
-      <section>
-        <button class="dice">Lanzar Dado</button>
-        <div class="game-status">En curso</div>
-      </section>
+        <section>
+          <button class="dice">Lanzar Dado</button>
+          <div class="game-status">En curso</div>
+        </section>
 
-      <section class="goods-container">
-        <div class="goods-item">🍪</div>
-        <div class="goods-item">🍪</div>
-        <div class="goods-item">🍪</div>
-      </section>
-      <section class="goods-container">
-        <div class="goods-item">🥚</div>
-        <div class="goods-item">🥚</div>
-        <div class="goods-item">🥚</div>
-      </section>
-      <section class="goods-container">
-        <div class="goods-item">🐸</div>
-        <div class="goods-item">🐸</div>
-        <div class="goods-item">🐸</div>
-      </section>
-      <section>
-        <button class="restart-button">Reiniciar Juego</button>
-      </section>
-    </main>quí tu código HTML</p>
+        <section class="goods-container">
+          <div class="goods-item">🍪</div>
+          <div class="goods-item">🍪</div>
+          <div class="goods-item">🍪</div>
+        </section>
+        <section class="goods-container">
+          <div class="goods-item">🥚</div>
+          <div class="goods-item">🥚</div>
+          <div class="goods-item">🥚</div>
+        </section>
+        <section class="goods-container">
+          <div class="goods-item">🐸</div>
+          <div class="goods-item">🐸</div>
+          <div class="goods-item">🐸</div>
+        </section>
+        <section>
+          <button class="restart-button">Reiniciar Juego</button>
+        </section>
+      </main>
+    </>
   );
 }
 
